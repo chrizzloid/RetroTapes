72dec6b3f55a2801fc18eb31cba2a93563858dac
Thomas Tollstedt <thomastollstedt@gmail.com>
2025-09-04 09:11:14 +0200

    Scaffolden SakilaDb, added connectionstring 'SakilaDb' and registered SakilaContext in program.cs

---
+++ Models/Log.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RetroTapes.Models;
+
+public partial class Log
+{
+    public int Id { get; set; }
+
+    public string? LogText { get; set; }
+}
---
+++ Models/VwActiveCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RetroTapes.Models;
+
+public partial class VwActiveCustomer
+{
+    public int CustomerId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Active { get; set; } = null!;
+}
