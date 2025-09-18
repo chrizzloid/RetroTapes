f5ceb29d6f0caa7224238a396d5c095099797f34
AlbinFM <albin.fagerstrom@live.se>
2025-09-18 09:58:05 +0200

    Flytta VM till rätt mapp samt delade upp logiken till en StaffService som även fick ett interface för lösare koppling till databasen.

---
+++ Services/IStaffService.cs
@@ -0,0 +1,6 @@
+﻿namespace RetroTapes.Services
+{
+    public interface IStaffService
+    {
+    }
+}
---
+++ Services/StaffService.cs
@@ -0,0 +1,6 @@
+﻿namespace RetroTapes.Services
+{
+    public class StaffService
+    {
+    }
+}
